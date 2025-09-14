36d8e2eb7c3433fe6f848ace4f89af1ee6edd85b
Th1y <tiago.pinheiro.git@gmail.com>
2025-09-14 19:36:00 -0300

    Lista de alteraçoes:
    - Rotas corrigidas e comentadas para facil entendimento
    - criado pasta public, que armazena os arquivos HTML
    - formulario.HTML criado, com formularios para Nome, Nota 1 e Nota 2

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
+// const port = 3000; // teste local, nao usado no app final
+
+// Middleware para interpretar dados do formulario
+app.use(express.urlencoded({ extended: true}));
 
 // Rotas para /, /sobre, /seu_problema e /resposta_problema.
 
@@ -13,7 +17,14 @@ app.get('/sobre', (req, res) => {
 
 })
 
-// Rota seu problema
-app.get('/sobre', (req, res) => {
+// Rota seu problema que vai exibir o HTML do formulario
+app.get('/seu_problema', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
 
+// Rota resposta problema que vai receber dados do HTML formulario
+app.post('/resposta_problema', (req, res) => {
+    // Array com os dados vindos do formulario
+    const { nome, nota1, nota2} = req.body;
+    res.send()
 })
