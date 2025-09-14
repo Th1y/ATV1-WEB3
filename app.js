018346ce027a7521eeb75c03793dcf6fdb69c59b
Th1y <tiago.pinheiro.git@gmail.com>
2025-09-14 18:06:40 -0300

    Importa Express.js, cria app.js e rotas principais

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Rotas para /, /sobre, /seu_problema e /resposta_problema.
+
+// Rota raiz
+app.get('/', (req, res) => {
+    
+})
+// Rota sobre
+app.get('/sobre', (req, res) => {
+
+})
+
+// Rota seu problema
+app.get('/sobre', (req, res) => {
+
+})
